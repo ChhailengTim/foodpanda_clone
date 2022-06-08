e07e675c9245b2dbfe82fb52e144ab5ffae687ce
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-09 00:05:28 +0700

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:foodpanda_clone/utils/default_color.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: DefaultColor.backgroundColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.list,
+          ),
+          onPressed: () {},
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("St 2002"),
+            Text(
+              "Phnom Penh",
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.card_travel),
+          ),
+        ],
+      ),
+    );
+  }
+}
