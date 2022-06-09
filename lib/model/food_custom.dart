03180cdc611392f9ca2fa45a2129984b765586d6
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-09 14:54:39 +0700

    Create food_custom.dart

---
+++ lib/model/food_custom.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FoodCustom extends StatelessWidget {
+  const FoodCustom({
+    Key? key,
+    this.text,
+    this.subtext,
+  }) : super(key: key);
+
+  final String? text;
+  final String? subtext;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text ?? "Food delivery",
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                fontWeight: FontWeight.w800,
+                fontSize: 25,
+              ),
+        ),
+        Text(
+          subtext ?? "Food delivery",
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Colors.black54,
+                fontSize: 12,
+              ),
+        ),
+      ],
+    );
+  }
+}
