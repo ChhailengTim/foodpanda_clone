40b780ee09f0275d483b6ad6c8bb22393f47133c
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-10 00:26:53 +0700

    Create deals_controller.dart

---
+++ lib/controller/deals_controller.dart
@@ -0,0 +1,22 @@
+import 'package:foodpanda_clone/model/deals_model.dart';
+import 'package:get/get.dart';
+
+class DealsController extends GetxController {
+  final deals = <DealsModel>[
+    DealsModel(img: "image/deals/c1.png"),
+    DealsModel(img: "image/deals/c2.png"),
+    DealsModel(img: "image/deals/c3.png"),
+    DealsModel(img: "image/deals/c4.png"),
+    DealsModel(img: "image/deals/c5.png"),
+    DealsModel(img: "image/deals/c6.png"),
+    DealsModel(img: "image/deals/c7.png"),
+    DealsModel(img: "image/deals/c8.png"),
+    DealsModel(img: "image/deals/c9.png"),
+    DealsModel(img: "image/deals/c10.png"),
+    DealsModel(img: "image/deals/c11.png"),
+    DealsModel(img: "image/deals/c12.png"),
+    DealsModel(img: "image/deals/c13.png"),
+    DealsModel(img: "image/deals/c14.png"),
+    DealsModel(img: "image/deals/c15.png"),
+  ];
+}
