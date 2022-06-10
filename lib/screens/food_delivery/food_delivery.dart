a082cf79e4fb9f4b495e16afb4a0ba1efca3a55e
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-11 00:22:51 +0700

    Update food_delivery.dart

--- lib/screens/food_delivery/food_delivery.dart
+++ lib/screens/food_delivery/food_delivery.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foodpanda_clone/controller/deals_controller.dart';
 import 'package:foodpanda_clone/utils/default_color.dart';
+import 'package:get/get.dart';
 
 class FoodDelivery extends StatelessWidget {
   const FoodDelivery({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final DealsController dealsController = Get.put(DealsController());
     return Scaffold(
       backgroundColor: DefaultColor.backgroundColor,
       appBar: AppBar(
@@ -93,6 +96,102 @@ class FoodDelivery extends StatelessWidget {
                       ),
                     ],
                   ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: dealsController.deals.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Row(
+                            children: [
+                              Image.asset(
+                                '${dealsController.deals[index].img}',
+                                height: 200,
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Cuisines",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 90,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: dealsController.cuisines.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Row(
+                            children: [
+                              Stack(
+                                children: [
+                                  Image.asset(
+                                    '${dealsController.cuisines[index].imgc}',
+                                    height: 90,
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.only(
+                                      left: 10,
+                                      top: 55,
+                                    ),
+                                    width: 70,
+                                    child: Text(
+                                      dealsController.cuisines[index].title
+                                          .toString(),
+                                      style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 7,
+                              ),
+                              Stack(
+                                children: [
+                                  Image.asset(
+                                    '${dealsController.cuisines2[index].imgc}',
+                                    height: 90,
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.only(
+                                      left: 10,
+                                      top: 55,
+                                    ),
+                                    width: 70,
+                                    child: Text(
+                                      dealsController.cuisines2[index].title
+                                          .toString(),
+                                      style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
                 ),
                 const Text(
                   "data",
