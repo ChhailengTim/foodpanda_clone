6f1611f14200ae2c3db73ab978aaeb759ffacb79
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-12 01:00:58 +0700

    Create pandamart.dart

---
+++ lib/screens/pandamart/pandamart.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PandamartScreen extends StatelessWidget {
+  const PandamartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.pink,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text(
+          "Pandamart",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.card_travel,
+              color: Colors.pink,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
