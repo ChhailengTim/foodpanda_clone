5076a2d5f64175c14d2a334c7306370c6863b1c1
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-06-11 11:07:22 +0700

    Update food_custom.dart

--- lib/model/food_custom.dart
+++ lib/model/food_custom.dart
@@ -5,36 +5,31 @@ class FoodCustom extends StatelessWidget {
     Key? key,
     this.text,
     this.subtext,
-    this.onTap,
   }) : super(key: key);
 
   final String? text;
   final String? subtext;
-  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            text ?? "Food delivery",
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 25,
-                ),
-          ),
-          Text(
-            subtext ?? "",
-            style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: Colors.black54,
-                  fontSize: 12,
-                ),
-          ),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text ?? "Food delivery",
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                fontWeight: FontWeight.w800,
+                fontSize: 25,
+              ),
+        ),
+        Text(
+          subtext ?? "",
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Colors.black54,
+                fontSize: 12,
+              ),
+        ),
+      ],
     );
   }
 }
