3a6df6bae8b097877f39a317e78dc86245e16128
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-12 17:53:27 +0700

    Create shops_items_head.dart

---
+++ lib/screens/shops/shops_items_head.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ShopsItems extends StatelessWidget {
+  const ShopsItems({
+    Key? key,
+    this.img,
+    this.title,
+  }) : super(key: key);
+  final String? img;
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: const BoxDecoration(
+            color: Colors.black26,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: Image.network(
+              img ?? "",
+              height: 30,
+            ),
+          ),
+        ),
+        Text(
+          title ?? "",
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
+  }
+}
