19d1b719f014d14f61043464ae76795df10c0868
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-14 00:06:16 +0700

    Update pandamart.dart

--- lib/screens/pandamart/pandamart.dart
+++ lib/screens/pandamart/pandamart.dart
@@ -1,8 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foodpanda_clone/controller/deals_controller.dart';
 import 'package:foodpanda_clone/controller/slidermodel_controller.dart';
 import 'package:foodpanda_clone/utils/default_color.dart';
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class PandamartScreen extends StatefulWidget {
   const PandamartScreen({Key? key}) : super(key: key);
@@ -14,9 +17,8 @@ class PandamartScreen extends StatefulWidget {
 class _PandamartScreenState extends State<PandamartScreen> {
   @override
   Widget build(BuildContext context) {
-    final SliderController sliderController = Get.put(
-      SliderController(),
-    );
+    final SliderController sliderController = Get.put(SliderController());
+    final DealsController dealsController = Get.put(DealsController());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -41,95 +43,324 @@ class _PandamartScreenState extends State<PandamartScreen> {
           ),
         ],
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            floating: true,
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(16),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 12.0,
-                  right: 12.0,
-                  bottom: 12.0,
-                ),
-                child: CupertinoTextField(
-                  obscureText: false,
-                  placeholder: "Search for restaurants, cuisines, and dishes",
-                  prefix: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(Icons.search),
+      body: Obx(
+        () => CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              floating: true,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(16),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 12.0,
+                    right: 12.0,
+                    bottom: 12.0,
                   ),
-                  decoration: BoxDecoration(
-                    color: DefaultColor.backgroundColor,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  suffix: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.display_settings,
-                      color: Colors.pink,
-                      size: 25,
+                  child: CupertinoTextField(
+                    obscureText: false,
+                    placeholder: "Search for restaurants, cuisines, and dishes",
+                    prefix: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(Icons.search),
+                    ),
+                    decoration: BoxDecoration(
+                      color: DefaultColor.backgroundColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    suffix: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.display_settings,
+                        color: Colors.pink,
+                        size: 25,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.warning_amber_rounded,
-                        color: Colors.pink,
-                      ),
-                      const Text("Shop information"),
-                      const Spacer(),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "View",
-                          style: TextStyle(color: Colors.pink),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.warning_amber_rounded,
+                          color: Colors.pink,
                         ),
-                      ),
-                    ],
-                  ),
-                  Card(
-                    elevation: 10,
-                    child: SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: sliderController.slide.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              child: Container(
-                                width: 250,
+                        const Text("Shop information"),
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View",
+                            style: TextStyle(color: Colors.pink),
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Card(
+                    //   elevation: 10,
+                    //   child: SizedBox(
+                    //     height: 200,
+                    //     child: ListView.builder(
+                    //         scrollDirection: Axis.horizontal,
+                    //         itemCount: sliderController.slide.length,
+                    //         itemBuilder: (context, index) {
+                    //           return Card(
+                    //             child: Container(
+                    //               width: 250,
+                    //               decoration: BoxDecoration(
+                    //                 border: Border.all(
+                    //                   color: Colors.black26,
+                    //                 ),
+                    //                 borderRadius: BorderRadius.circular(10),
+                    //                 image: DecorationImage(
+                    //                   image: NetworkImage(
+                    //                       '${sliderController.slide[index].image}'),
+                    //                   fit: BoxFit.cover,
+                    //                 ),
+                    //               ),
+                    //             ),
+                    //           );
+                    //         }),
+                    //   ),
+                    // ),
+                    Center(
+                      child: Column(
+                        children: [
+                          CarouselSlider.builder(
+                            itemCount: sliderController.slide.length,
+                            itemBuilder: (context, index, realIndex) {
+                              return Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 5,
+                                ),
                                 decoration: BoxDecoration(
                                   border: Border.all(
-                                    color: Colors.black26,
+                                    color: Colors.black38,
+                                    width: 1,
+                                    style: BorderStyle.solid,
                                   ),
                                   borderRadius: BorderRadius.circular(10),
                                   image: DecorationImage(
                                     image: NetworkImage(
-                                        '${sliderController.slide[index].image}'),
+                                      '${sliderController.slide[index].image}',
+                                    ),
                                     fit: BoxFit.cover,
                                   ),
                                 ),
+                              );
+                            },
+                            options: CarouselOptions(
+                              autoPlay: true,
+                              autoPlayInterval: const Duration(
+                                seconds: 10,
                               ),
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  sliderController.activeIndex = index;
+                                });
+                              },
+                              height: 150,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          AnimatedSmoothIndicator(
+                            activeIndex: sliderController.activeIndex,
+                            count: sliderController.slide.length,
+                            effect: const WormEffect(
+                              activeDotColor: Colors.pink,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          GridView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: sliderController.viewMore.value
+                                  ? sliderController.gridItem.length
+                                  : 16,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 4,
+                                crossAxisSpacing: 5,
+                                mainAxisSpacing: 5,
+                              ),
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 50,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.black12,
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: Center(
+                                        child: Image.network(
+                                          '${sliderController.gridItem[index].image}',
+                                          height: 30,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      '${sliderController.gridItem[index].tilte}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                            ),
+                            onPressed: () {
+                              sliderController.viewMore.value =
+                                  !sliderController.viewMore.value;
+                            },
+                            child: sliderController.viewMore.value
+                                ? Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Icon(
+                                        Icons.keyboard_arrow_up,
+                                        color: Colors.pink,
+                                      ),
+                                      Text(
+                                        "View less",
+                                        style: TextStyle(
+                                          color: Colors.pink,
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                : Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const [
+                                      Icon(
+                                        Icons.keyboard_arrow_down,
+                                        color: Colors.pink,
+                                      ),
+                                      Text(
+                                        "View all cataegories",
+                                        style: TextStyle(
+                                          color: Colors.pink,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                          ),
+                          CarouselSlider.builder(
+                            itemCount: sliderController.slide.length,
+                            itemBuilder: (context, index, realIndex) {
+                              return Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black38,
+                                    width: 1,
+                                    style: BorderStyle.solid,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      '${sliderController.slide[index].image}',
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              );
+                            },
+                            options: CarouselOptions(
+                              autoPlay: true,
+                              autoPlayInterval: const Duration(
+                                seconds: 10,
+                              ),
+                              onPageChanged: (index, reason) {
+                                setState(() {
+                                  sliderController.activeIndex2 = index;
+                                });
+                              },
+                              height: 150,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          AnimatedSmoothIndicator(
+                            activeIndex: sliderController.activeIndex2,
+                            count: sliderController.slide.length,
+                            effect: const WormEffect(
+                              activeDotColor: Colors.pink,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "New Store",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: dealsController.deals.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Image.asset(
+                                  "${dealsController.deals[index].img}"),
                             );
                           }),
                     ),
-                  ),
-                ],
-              ),
-            ]),
-          )
-        ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Newbie Town",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: dealsController.deals.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Image.asset(
+                                  "${sliderController.deals[index].img}"),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
